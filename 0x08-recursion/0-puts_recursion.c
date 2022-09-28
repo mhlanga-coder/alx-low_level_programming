adc09b106dd5717b3417336ca8a404c01fc7fb6f
mhlanga-coder <royzar08@gmail.com>
2022-09-28 10:28:29 -0700

    0-puts_recursion.c update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		_putchar(s[0]);
-		_puts_recursion(s +l);
+		_puts_recursion(s + l);
 	}
 }
+
