b164c06309180503b811b00209b0db8e4109b271
mhlanga-coder <royzar08@gmail.com>
2022-09-30 14:48:09 -0700

    4-add.c update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
-			if (isdigit(argv[i][j]) == 0
+			if (isdigit(argv[i][j]) == 0)
 			{
 				puts("Error");
 				return (1);
